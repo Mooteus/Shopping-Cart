db2bc4b7be333f84a9e768609d92012c971b9527
Mooteus <moteus10@gmail.com>
2022-03-24 19:50:09 -0300

    initial commit

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
