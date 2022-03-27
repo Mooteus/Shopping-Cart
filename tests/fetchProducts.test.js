d2caedf617595f63989a123b1e006060c7dd8672
Mooteus <moteus10@gmail.com>
2022-03-27 16:18:27 -0300

    tests: fix async and verify return of fetchProducts

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,12 +3,21 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  it('Verifica se fetchProducts é uma Função', () => {
+  it('Verifica se fetchProducts é uma Função', async () => {
     expect(typeof fetchProducts).toBe('function');
   });
 
-  it(`Verifica se a função fetchProducts chama fetch ao passar o argumento "computador"`, () => {
-    fetchProducts('computador');
+  it(`Verifica se a função fetchProducts chama fetch ao passar o argumento "computador"`, async () => {
+    await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
+  });
+
+  it(`Verifica se a função fetch utiliza o end-point "https://api.mercadolibre.com/sites/MLB/search?q=computador"`, async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('Verifica se o retorno de fetchProducts é igual a computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toBe(computadorSearch);
   });
 });
