81e67d4c67d37bff006ff7f517007f96979c5870
Mooteus <moteus10@gmail.com>
2022-03-27 15:55:58 -0300

    test: check if fetchProducts call fetch with computer argument

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,4 +6,9 @@ describe('1 - Teste a função fecthProducts', () => {
   it('Verifica se fetchProducts é uma Função', () => {
     expect(typeof fetchProducts).toBe('function');
   });
+
+  it(`Verifica se a função fetchProducts chama fetch ao passar o argumento "computador"`, () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
