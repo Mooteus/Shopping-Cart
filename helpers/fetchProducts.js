93502dc8aacfc298ba89735c49a848a1cae4412d
Mooteus <moteus10@gmail.com>
2022-03-27 16:03:25 -0300

    fix: fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,9 @@
-const fetchProducts = async () => {
-  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+const fetchProducts = async (querry) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${querry}`;
 
   try {
-    const response = fetch(url);
-    const data = response.json();
+    const response = await fetch(url);
+    const data = await response.json();
     return data;
   } catch (error) {
     throw new Error(error);
