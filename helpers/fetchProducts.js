c5de555f05b765ad72b0ee6977e2395e255c3b5d
Mooteus <moteus10@gmail.com>
2022-03-27 14:28:52 -0300

    feat(helpers/fetchProducts): receive api data

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+
+  try {
+    const response = fetch(url);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 if (typeof module !== 'undefined') {
