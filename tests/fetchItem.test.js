a93016165cc56188a75712240377bbe14b1985b7
Mooteus <moteus10@gmail.com>
2022-03-29 08:00:24 -0300

    test: check if fetchItem is a function

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,7 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
 });
