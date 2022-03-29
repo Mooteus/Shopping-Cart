727703e31da5afb4944b68202e97f50f4e4b2d45
Mooteus <moteus10@gmail.com>
2022-03-29 08:08:40 -0300

    test: check return of fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,7 +3,7 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  it('Verifica se fetchItem é uma função', () => {
+  it('Verifica se fetchItem é uma função', async () => {
     expect(typeof fetchItem).toBe('function');
   });
 
@@ -15,5 +15,9 @@ describe('2 - Teste a função fecthItem', () => {
   it('Verifique se a função fetchItem utiliza o endPoint correto', async () => {
     await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+
+  it('Verifica se o retorno de fetchItem é o esperado', async () => {
+    expect(await fetchItem('MLB1615760527')).toBe(item);
   });
 });
