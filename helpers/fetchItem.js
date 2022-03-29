e667d608a6eeb642e5aa5ac4425869e0d0b68b6a
Mooteus <moteus10@gmail.com>
2022-03-29 11:42:10 -0300

    feat: check if function receive a argument

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,6 +2,7 @@ const fetchItem = async (id) => {
    const url = `https://api.mercadolibre.com/items/${id}`;
 
    try {
+     if (!id) throw new Error('You must provide an url');
      const response = await fetch(url);
      const data = await response.json();
      return data;
