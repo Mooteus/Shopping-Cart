f1e28b07f99c2ca10ec9447c7b7b7e0dd508a250
Mooteus <moteus10@gmail.com>
2022-03-29 08:01:06 -0300

    test: check if fetch is called

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,4 +6,9 @@ describe('2 - Teste a função fecthItem', () => {
   it('Verifica se fetchItem é uma função', () => {
     expect(typeof fetchItem).toBe('function');
   });
+
+  it('Execute a função fetchItem e verifique se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
 });
