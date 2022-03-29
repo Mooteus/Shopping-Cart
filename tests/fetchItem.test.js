272da884226178565c8884fcf6b46341e0510ced
Mooteus <moteus10@gmail.com>
2022-03-29 11:47:12 -0300

    test: check call fetchItem without argument

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -20,4 +20,8 @@ describe('2 - Teste a função fecthItem', () => {
   it('Verifica se o retorno de fetchItem é o esperado', async () => {
     expect(await fetchItem('MLB1615760527')).toBe(item);
   });
+
+  it('Verifica se ao chamar fetchItem sem argumento é retornado um erro', async () => {
+    await fetchItem().catch(error => expect(error).toEqual(new Error('You must provide an url')));
+  });
 });
