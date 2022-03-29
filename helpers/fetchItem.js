813d62c3248d0fb8a47284fdff9f627f36e99e11
Mooteus <moteus10@gmail.com>
2022-03-28 21:41:05 -0300

    feat: search for item data with your id

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+   const url = `https://api.mercadolibre.com/items/${id}`;
+
+   try {
+     const response = await fetch(url);
+     const data = await response.json();
+     return data;
+   } catch (error) {
+     throw new Error(error);
+   } 
 };
 
 if (typeof module !== 'undefined') {
