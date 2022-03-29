db968fdfec65a028a964e6b232da9e126a1ae0d0
Mooteus <moteus10@gmail.com>
2022-03-28 22:15:41 -0300

    fix: check url

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,8 @@
 const fetchProducts = async (querry) => {
-  if (!querry) throw new Error('You must provide an url');
-
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${querry}`;
 
   try {
+    if (!querry) throw new Error('You must provide an url');
     const response = await fetch(url);
     const data = await response.json();
     return data;
