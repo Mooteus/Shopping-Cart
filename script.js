2a80956442c66485974bfc65d037519fd4eabfb9
Mooteus <moteus10@gmail.com>
2022-04-02 17:20:31 -0300

    feat: calcule total price

--- script.js
+++ script.js
@@ -20,11 +20,29 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function deleteItemFromCart(event, id) {
+let totalPrice = 0;
+if (localStorage.getItem('totalPrice')) totalPrice = parseFloat(localStorage.getItem('totalPrice'));
+async function updateTotalPrice(val, type) {
+  if (type === 'add') {
+    totalPrice += val;
+  } else if (type === 'sub') {
+    totalPrice -= val;
+  }
+  localStorage.setItem('totalPrice', totalPrice);
+  document.querySelector('.total-price').innerText = totalPrice;
+}
+
+async function ReloadTotalPrice() {
+  document.querySelector('.total-price').innerText = totalPrice;
+}
+
+async function deleteItemFromCart(event, id) {
   event.target.parentNode.removeChild(event.target);
   const removeId = saveCartItem.indexOf(id);
   saveCartItem.splice(removeId, 1);
   localStorage.setItem('cart', JSON.stringify(saveCartItem));
+  const product = await fetchItem(id);
+  updateTotalPrice(product.price, 'sub');
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -54,6 +72,7 @@ async function addToCart(e) {
   const objReturn = { sku: id, name: product.title, salePrice: product.price };
   cartContainer.appendChild(createCartItemElement(objReturn));
   saveCart(product.id);
+  updateTotalPrice(product.price, 'add');
 }
 
 function createProductItemElement({ sku, name, image }) {
@@ -86,6 +105,9 @@ function clearCart() {
   
   saveCartItem = [];
   localStorage.setItem('cart', JSON.stringify(saveCartItem));
+  totalPrice = 0;
+  localStorage.setItem('totalPrice', totalPrice);
+  ReloadTotalPrice();
 }
 
 const clearCartClick = document.querySelector('.empty-cart');
@@ -94,4 +116,5 @@ clearCartClick.addEventListener('click', clearCart);
 window.onload = () => { 
   renderItem('computador');
   loadCart();
+  ReloadTotalPrice();
 };
