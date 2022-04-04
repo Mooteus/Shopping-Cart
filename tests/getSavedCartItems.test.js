58419734f828da1e2deca0071aea9884834856ab
Mooteus <moteus10@gmail.com>
2022-04-04 02:51:53 -0300

    test: getSavedCartItems call localstorage and cartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,9 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('verifica se ao chamar getSavedCartItems é chamado localStorage.getItem', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+    expect(localStorage.getItem).toHaveBeenCalled('cartItems');
+  });
 });
